3edd514c826ba3c1e00cee3637f007c63c5d36e8
vamsi <vamsimaram7007@gmail.com>
2025-01-30 21:03:04 -0500

    Initial Commit

---
+++ app/components/About.jsx
@@ -0,0 +1,112 @@
+import {assets, infoList, toolsData} from '@/assets/assets' 
+import { motion } from "motion/react"
+import Image from 'next/image'
+import React from 'react'
+
+const About = ({isDarkMode}) => {
+  return (
+    <motion.div 
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+    id='about' className='w-full px-[12%] py-10
+    scroll-mt-20 flex flex-col items-center text-center'>
+      <motion.h4 
+      initial={{opacity:0, y:-20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.3}}
+      className='text-center mb-2 text-lg font-Ovo'>
+      Introduction
+      </motion.h4>
+
+      <motion.h2 
+      initial={{opacity:0, y:-20}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration:0.5, delay:0.5}}
+      className='text-center text-5xl font-Ovo'>
+      About Me
+      </motion.h2>
+
+      <motion.div 
+      initial={{opacity:0}}
+      whileInView={{opacity:1}}
+      transition={{duration:0.8}}
+      className='max-w-2xl mt-10'>
+        {/* <motion.div 
+        initial={{opacity:0,scale:0.9}}
+        whileInView={{opacity:1, scale:1}}
+        transition={{duration:0.6}}
+        className='w-64 sm:w-80 rounded-3xl max-w-none'>
+            <Image src={assets.user_image} alt=''
+                className='w-full rounded-3xl'/>
+        </motion.div> */}
+
+        <motion.div
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:0.6, delay:0.8}}
+         className='flex-1 justify-center'>
+           <p className='mb-10 max-w-2xl font-Ovo justify-center '>
+           I am a frontend developer currently based in Washington, USA. 
+            With a Master's in Computer Science from George Mason University, 
+            I specialize in web development, data science, and software development. 
+            I'm passionate about using my technical skills to create intuitive and impactful web applications.
+
+           {/* I am a frontend web developer based in Washington, USA. With a 
+           passion for building dynamic and responsive websites, I specialize 
+           in creating seamless user experiences. I thrive in fast-paced environments, '
+           and I am always eager to learn new technologies to improve my skills. */}
+           </p> 
+
+           <motion.ul 
+           initial={{opacity:0}}
+           whileInView={{opacity:1}}
+           transition={{duration:0.8, delay:1}}
+           className='grid grid-cols-1 sm:grid-cols-3 gap-6
+           max-w-2xl justify-center'>
+            {infoList.map(({icon,iconDark, title, description},index)=>(
+                <motion.li 
+                whileInView={{scale:1.05}}
+                className='border-[0.5px] border-gray-400 justify-center
+                rounded-xl p-6 cursor-pointer hover:bg-lightHover
+                hover:-translate-y-1 duration-500 hover:shadow-black
+                dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
+                key={index}>
+                <Image src={isDarkMode? iconDark : icon} alt='' className='w-7 mt-3 justify-center'/>
+                <h3 className='my-4 font-semibold
+                text-gray-700 dark:text-white'>{title}</h3>
+                <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
+                </motion.li>
+            ))}
+           </motion.ul>
+           <motion.h4
+           initial={{opacity:0, y:20}}
+           whileInView={{opacity:1, y:0}}
+           transition={{duration:0.5, delay:1.3}}
+            className='my-6 justify-center text-gray-700 font-Ovo dark:text-white/80'>Tools
+            </motion.h4>
+
+           <motion.ul 
+           initial={{opacity:0}}
+           whileInView={{opacity:1}}
+           transition={{duration:0.6, delay:1.5}}  
+           className='flex justify-center items-center gap-3 sm:gap-5'>
+             {toolsData.map((tool,index)=>(
+                <motion.li 
+                whileInView={{scale:1.05}}
+                className='flex items-center justify-center
+                w-12 sm:w-14 aspect-square border border-gray-400
+                rounded-lg cursor-pointer hover:-translate-y-1
+                duration-500' 
+                key={index}>
+                    <Image src={tool} alt='' className='w-5 sm:w-7'/>
+                </motion.li>
+             ))}
+           </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default About
---
+++ app/components/Header.jsx
@@ -0,0 +1,79 @@
+import {assets} from '@/assets/assets' 
+import { motion } from "motion/react"
+import Image from 'next/image'
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col 
+    items-center justify-center gap-4'>
+      {/* <motion.div 
+       initial={{scale:0}}
+       whileInView={{scale:1}}
+       transition={{duration:0.8, type:'spring', stiffness:100}}
+      >
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </motion.div> */}
+
+      <motion.h3 
+      initial={{y:-20, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:0.6, delay:0.3}}
+      className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+      Hi! I'm Vamsi Krishna Maram <Image src={assets.hand_icon} alt=''
+        className='w-6'/></motion.h3>
+        <motion.h1 
+        initial={{y:-30, opacity:0}}
+        whileInView={{y:0, opacity:1}}
+        transition={{duration:0.8, delay:0.5}}
+        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+        full-stack web developer based in Washington.
+        </motion.h1>
+
+        <motion.p 
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:0.6, delay:0.7}}
+        className='max-w-2xl mx-auto font-Ovo'>
+          I am a full-stack web developer based in Washington, USA. With a 
+           passion for building dynamic and responsive websites, I specialize 
+           in creating seamless user experiences. I thrive in fast-paced environments, '
+           and I am always eager to learn new technologies to improve my skills.
+
+            {/* I am a frontend developer currently based in Washington, USA. 
+            With a Master's in Computer Science from George Mason University, 
+            I specialize in web development, data science, and software development. 
+            In addition to my academic background, I have practical experience 
+            as a Machine Learning Research Assistant, where I built AI-driven 
+            systems like a smart traffic signal control system. I'm passionate about 
+            using my technical skills to create intuitive and impactful web applications. */}
+        </motion.p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+
+            <motion.a 
+            initial={{y:30, opacity:0}}
+            whileInView={{y:0, opacity:1}}
+            transition={{duration:0.6, delay:1}}
+            href="#contact"
+            className='px-10 py-3 border border-white rounded-full bg-black
+            text-white flex items-center gap-2 dark:bg-transparent'
+            >contact me <Image src={assets.right_arrow_white}
+                alt='' className='w-4'/>
+            </motion.a>
+
+            <motion.a 
+            initial={{y:30, opacity:0}}
+            whileInView={{y:0, opacity:1}}
+            transition={{duration:0.6, delay:1.2}}
+            href="/Vamsi_Resume.pdf" download
+            className='px-10 py-3 border rounded-full border-gray-500 flex
+            items-center gap-2 bg-white dark:text-black'>
+            my resume <Image src={assets.download_icon} alt='' className='w-4'/>
+            </motion.a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
